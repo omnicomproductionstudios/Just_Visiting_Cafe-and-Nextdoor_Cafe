68527a78b39a40134729c72d4d62bcedec8d61a7
Mahesh Bhatt <mahesh.bhatt@omc.com>
2026-04-15 16:37:44 +0530

    xdfg

---
+++ Nextdoor_Cafe/Bank_Bank_Nextdoor_Cafe_Rev1_NA_RM_RM_EN_NA_1280x100_P00119876/manifest.js
@@ -0,0 +1,14 @@
+FT.manifest({
+	"filename":"Bank_Just_Visiting_No_Fees_NA_RM_RM_EN_NA_1280x100_P00114026.html",
+	"width":1280,
+	"height":100,
+	"clickTagCount":1,
+  "videos":[{"name":"video1", "ref":"230245/TUBK1566000H_BANK_PARENT_WEEK_NOFEES_NA_VID_VID_EN_15_16X9_TUBK1566000H_P00115795_OLV_HD_20260205_H264_Unslated_GOOGLE_YOUTUBE_ORGANIC"}],
+	"expand":{
+		"fullscreen":false,
+		"width":1280,
+		"height":418,
+		"indentAcross":0,
+		"indentDown":0
+  }
+});
